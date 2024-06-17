ae94aa54a85ea41b2f0f82ab8e7948dd2831448c
sbv2605@gmail.com <sbv2605@gmail.com>
2024-06-17 09:28:04 +0300

    with exception class

---
+++ Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex03.GarageLogic
+{
+    public class ValueOutOfRangeException : Exception
+    {
+        private float m_MaxValue;
+        private float m_MinValue;
+        public ValueOutOfRangeException(float i_MaxValue, float i_MinValue)
+            : base(string.Format("The value needs to be in the range {0} to {1}", i_MinValue, i_MaxValue))
+        {
+            m_MinValue = i_MinValue;
+            m_MaxValue = i_MaxValue;
+        }
+
+        float MaxValue { get { return m_MaxValue; } }
+        float MinValue { get { return m_MinValue; } }
+    }
+}
